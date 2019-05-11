014c4876e35a992f191ab7592598a08143a9fea0
emma lapalm <lillypadcrafts@gmail.com>
2019-05-11 08:28:06 -0700

    still working with Travis take 22

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,9 @@ module.exports = function(app) {
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+    db.Example.create(req.body).then(function(
+      dbExample
+      ) {
       res.json(dbExample);
     });
   });
