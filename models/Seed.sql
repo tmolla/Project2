70730260d3a5ad985761ab717d04c178f3f45d1b
Telahoun Molla <telahounmolla@gmail.com>
2019-05-16 21:21:51 -0700

    latest updates

---
+++ models/Seed.sql
@@ -0,0 +1,27 @@
+--all the vegitables and fruits we ever need
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Kale", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Potato", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Orange", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Carrot", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Tomatoe", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Lettuce", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Apple", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Arichoke", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Eggplant", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Beans", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Peas", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Broccoli", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Brussels sprouts", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Celery", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Cauliflower", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Chard", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Spinach", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Bok choy", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Okra", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Onions", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Parsley", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Bell pepper", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Chili pepper", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Radish", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Turnip", NOW(), NOW());
+INSERT INTO harvests (type, createdAt, updatedAt) VALUES ("Mellon", NOW(), NOW());
